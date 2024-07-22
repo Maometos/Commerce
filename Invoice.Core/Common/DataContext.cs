5b9812c9093584e9a2e73fcad4a6a6b0dcc84302
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2024-07-22 05:36:58 -0400

    Create a common Location entity

--- Invoice.Core/Common/DataContext.cs
+++ Invoice.Core/Common/DataContext.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Invoice.Core.Common.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Invoice.Core.Common;
 
 public class DataContext : DbContext
 {
+    public DbSet<Location> Locations { get; set; }
+
     public DataContext(DbContextOptions<DataContext> options) : base(options)
     {
     }
