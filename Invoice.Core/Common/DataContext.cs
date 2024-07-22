59713e29dd778c9f681591aaaed4c7385a0dcb2a
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2024-07-22 07:45:12 -0400

    Create the Enterprise entity

--- Invoice.Core/Common/DataContext.cs
+++ Invoice.Core/Common/DataContext.cs
@@ -1,10 +1,12 @@
 ﻿using Invoice.Core.Common.Entities;
+using Invoice.Core.Organization.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace Invoice.Core.Common;
 
 public class DataContext : DbContext
 {
+    public DbSet<Enterprise> Enterprises { get; set; }
     public DbSet<Location> Locations { get; set; }
 
     public DataContext(DbContextOptions<DataContext> options) : base(options)
---
+++ Invoice.Core/Organization/Entities/Enterprise.cs
@@ -0,0 +1,7 @@
+﻿using Invoice.Core.Common.Abstractions;
+
+namespace Invoice.Core.Organization.Entities;
+
+public class Enterprise : Profile
+{
+}
