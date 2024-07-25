019bc56f79839ecfdd3f861bafa75ff7926ea47b
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2024-07-25 18:15:54 -0400

    Create a common abstract TransactionTax class

---
+++ Invoice.Core/Common/Abstractions/TransactionTax.cs
@@ -0,0 +1,14 @@
+﻿using Invoice.Core.Organization.Entities;
+
+namespace Invoice.Core.Common.Abstractions;
+
+public abstract class TransactionTax
+{
+    public int Id { get; set; }
+    public int TaxId { get; set; }
+    public string Name { get; set; } = null!;
+    public double Rate { get; set; }
+    public decimal Amount { get; set; }
+
+    public Tax Tax { get; set; } = null!;
+}
