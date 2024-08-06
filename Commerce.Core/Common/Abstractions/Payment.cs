d0231ffd17d4a6349127507eac61837e3e0db3ac
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2024-08-05 20:28:49 -0400

    Create a common abstract Payment class

---
+++ Commerce.Core/Common/Abstractions/Payment.cs
@@ -0,0 +1,10 @@
+﻿using Commerce.Core.Common.Values;
+
+namespace Commerce.Core.Common.Abstractions;
+
+public abstract class Payment : Transaction
+{
+    public decimal InvoiceAmount { get; set; }
+    public decimal PaymentAmount { get; set; }
+    public PaymentMethod PaymentMethod { get; set; }
+}
