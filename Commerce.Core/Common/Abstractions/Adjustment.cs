e71846b1f31553c15bf83cad018ba92036a0c9d8
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2024-08-09 01:02:25 -0400

    Create a common abstract Adjustment class

---
+++ Commerce.Core/Common/Abstractions/Adjustment.cs
@@ -0,0 +1,10 @@
+﻿using Commerce.Core.Common.Values;
+
+namespace Commerce.Core.Common.Abstractions;
+
+public abstract class Adjustment : Transaction
+{
+    public decimal SubTotal { get; set; }
+    public decimal Total { get; set; }
+    public AdjustmentStatus Status { get; set; }
+}
