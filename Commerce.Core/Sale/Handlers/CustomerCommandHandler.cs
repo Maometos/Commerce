c519dc1a088c42e1410981e666a278bf2e82f864
Mohammed Moussaoui <m.moussaoui@outlook.com>
2024-08-18 16:15:15 -0400

    Implement Customer entity persisting - create CustomerCommand class - create CustomerCommandHandler class - create CustomerTest class

---
+++ Commerce.Core/Sale/Handlers/CustomerCommandHandler.cs
@@ -0,0 +1,65 @@
+﻿using Commerce.Core.Common;
+using Commerce.Core.Sale.Entities;
+using Commerce.Core.Sale.Requests;
+using Commerce.Infrastructure.CQRS;
+
+namespace Commerce.Core.Sale.Handlers;
+
+public class CustomerCommandHandler : CommandHandler<CustomerCommand>
+{
+    private DataContext context;
+
+    public CustomerCommandHandler(DataContext context)
+    {
+        this.context = context;
+    }
+
+    protected override async Task<int> CreateAsync(CustomerCommand command, CancellationToken token)
+    {
+        var item = new Customer();
+        item.Name = command.Name!;
+        item.Email = command.Email!;
+        item.Phone = command.Phone!;
+        item.Address = command.Address!;
+        item.Locality = command.Locality!;
+        item.Territory = command.Territory!;
+        item.Postcode = command.Postcode!;
+        item.Country = command.Country!;
+
+        context.Customers.Add(item);
+        return await context.SaveChangesAsync();
+    }
+
+    protected override async Task<int> UpdateAsync(CustomerCommand command, CancellationToken token)
+    {
+        var item = await context.Customers.FindAsync(command.Id, token);
+        if (item == null)
+        {
+            return 0;
+        }
+
+        item.Name = command.Name ?? item.Name;
+        item.Email = command.Email ?? item.Email;
+        item.Phone = command.Phone ?? item.Phone;
+        item.Address = command.Address ?? item.Address;
+        item.Locality = command.Locality ?? item.Locality;
+        item.Territory = command.Territory ?? item.Territory;
+        item.Postcode = command.Postcode ?? item.Postcode;
+        item.Country = command.Country ?? item.Country;
+
+        context.Customers.Update(item);
+        return await context.SaveChangesAsync(token);
+    }
+
+    protected override async Task<int> DeleteAsync(CustomerCommand command, CancellationToken token)
+    {
+        var item = await context.Customers.FindAsync(command.Id, token);
+        if (item == null)
+        {
+            return 0;
+        }
+
+        context.Customers.Remove(item);
+        return await context.SaveChangesAsync(token);
+    }
+}
---
+++ Commerce.Test/Sale/CustomerTest.cs
@@ -0,0 +1,76 @@
+﻿using Commerce.Core.Common;
+using Commerce.Core.Sale.Entities;
+using Commerce.Core.Sale.Handlers;
+using Commerce.Core.Sale.Requests;
+using Commerce.Infrastructure.CQRS;
+using Commerce.Infrastructure.Dispatcher;
+using Microsoft.EntityFrameworkCore;
+
+namespace Commerce.Test.Sale;
+
+public class CustomerTest
+{
+    private EventDispatcher dispatcher;
+    private DataContext context;
+
+    public CustomerTest()
+    {
+        var options = new DbContextOptionsBuilder<DataContext>()
+            .UseInMemoryDatabase(Guid.NewGuid().ToString())
+            .Options;
+
+        context = new DataContext(options);
+        dispatcher = new EventDispatcher();
+        dispatcher.AddService(context);
+        dispatcher.AddHandler<CustomerCommandHandler>();
+
+        var customer1 = new Customer() { Id = 1, Name = "John Doe", Email = "john.doe@email.com" };
+        var customer2 = new Customer() { Id = 2, Name = "Jane Doe", Email = "Jane.doe@email.com" };
+        var customer3 = new Customer() { Id = 3, Name = "John Smith", Email = "Jane.Smith@email.com" };
+        var customer4 = new Customer() { Id = 4, Name = "Jane Smith", Email = "Jane.Smith@email.com" };
+
+        context.Customers.Add(customer1);
+        context.Customers.Add(customer2);
+        context.Customers.Add(customer3);
+        context.Customers.Add(customer4);
+        context.SaveChanges();
+    }
+
+    [Fact]
+    public async void TestCreating()
+    {
+        var command = new CustomerCommand();
+        command.Action = CommandAction.Create;
+        command.Name = "Joe Blow";
+        command.Email = "joe.blow@email.com";
+
+        var result = await dispatcher.DispatchAsync(command);
+        Assert.Equal(1, result);
+    }
+
+    [Fact]
+    public async void TestUpdating()
+    {
+        var command = new CustomerCommand();
+        command.Action = CommandAction.Update;
+        command.Id = 4;
+        command.Address = "Fake street";
+
+        var result = await dispatcher.DispatchAsync(command);
+        Assert.Equal(1, result);
+
+        var customer = await context.Customers.FindAsync(4);
+        Assert.Equal("Fake street", customer!.Address);
+    }
+
+    [Fact]
+    public async void TestDeleting()
+    {
+        var command = new CustomerCommand();
+        command.Action = CommandAction.Delete;
+        command.Id = 1;
+
+        var result = await dispatcher.DispatchAsync(command);
+        Assert.Equal(1, result);
+    }
+}
