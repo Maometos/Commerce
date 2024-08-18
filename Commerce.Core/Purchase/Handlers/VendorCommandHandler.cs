ae2f3d59d89694b28020bdea1d967e914fec9239
Mohammed Moussaoui <m.moussaoui@outlook.com>
2024-08-18 16:32:34 -0400

    Implement Vendor entity persisting - create VendorCommand class - create VendorCommandHandler class - create CustomerTest class

---
+++ Commerce.Core/Purchase/Handlers/VendorCommandHandler.cs
@@ -0,0 +1,65 @@
+﻿using Commerce.Core.Common;
+using Commerce.Core.Purchase.Entities;
+using Commerce.Core.Purchase.Requests;
+using Commerce.Infrastructure.CQRS;
+
+namespace Commerce.Core.Purchase.Handlers;
+
+public class VendorCommandHandler : CommandHandler<VendorCommand>
+{
+    private DataContext context;
+
+    public VendorCommandHandler(DataContext context)
+    {
+        this.context = context;
+    }
+
+    protected override async Task<int> CreateAsync(VendorCommand command, CancellationToken token)
+    {
+        var item = new Vendor();
+        item.Name = command.Name!;
+        item.Email = command.Email!;
+        item.Phone = command.Phone!;
+        item.Address = command.Address!;
+        item.Locality = command.Locality!;
+        item.Territory = command.Territory!;
+        item.Postcode = command.Postcode!;
+        item.Country = command.Country!;
+
+        context.Vendors.Add(item);
+        return await context.SaveChangesAsync();
+    }
+
+    protected override async Task<int> UpdateAsync(VendorCommand command, CancellationToken token)
+    {
+        var item = await context.Vendors.FindAsync(command.Id, token);
+        if (item == null)
+        {
+            return 0;
+        }
+
+        item.Name = command.Name ?? item.Name;
+        item.Email = command.Email ?? item.Email;
+        item.Phone = command.Phone ?? item.Phone;
+        item.Address = command.Address ?? item.Address;
+        item.Locality = command.Locality ?? item.Locality;
+        item.Territory = command.Territory ?? item.Territory;
+        item.Postcode = command.Postcode ?? item.Postcode;
+        item.Country = command.Country ?? item.Country;
+
+        context.Vendors.Update(item);
+        return await context.SaveChangesAsync(token);
+    }
+
+    protected override async Task<int> DeleteAsync(VendorCommand command, CancellationToken token)
+    {
+        var item = await context.Vendors.FindAsync(command.Id, token);
+        if (item == null)
+        {
+            return 0;
+        }
+
+        context.Vendors.Remove(item);
+        return await context.SaveChangesAsync(token);
+    }
+}
---
+++ Commerce.Test/Purchase/VendorTest.cs
@@ -0,0 +1,76 @@
+﻿using Commerce.Core.Common;
+using Commerce.Core.Purchase.Entities;
+using Commerce.Core.Purchase.Handlers;
+using Commerce.Core.Purchase.Requests;
+using Commerce.Infrastructure.CQRS;
+using Commerce.Infrastructure.Dispatcher;
+using Microsoft.EntityFrameworkCore;
+
+namespace Commerce.Test.Purchase;
+
+public class VendorTest
+{
+    private EventDispatcher dispatcher;
+    private DataContext context;
+
+    public VendorTest()
+    {
+        var options = new DbContextOptionsBuilder<DataContext>()
+            .UseInMemoryDatabase(Guid.NewGuid().ToString())
+            .Options;
+
+        context = new DataContext(options);
+        dispatcher = new EventDispatcher();
+        dispatcher.AddService(context);
+        dispatcher.AddHandler<VendorCommandHandler>();
+
+        var customer1 = new Vendor() { Id = 1, Name = "John Doe", Email = "john.doe@email.com" };
+        var customer2 = new Vendor() { Id = 2, Name = "Jane Doe", Email = "Jane.doe@email.com" };
+        var customer3 = new Vendor() { Id = 3, Name = "John Smith", Email = "Jane.Smith@email.com" };
+        var customer4 = new Vendor() { Id = 4, Name = "Jane Smith", Email = "Jane.Smith@email.com" };
+
+        context.Vendors.Add(customer1);
+        context.Vendors.Add(customer2);
+        context.Vendors.Add(customer3);
+        context.Vendors.Add(customer4);
+        context.SaveChanges();
+    }
+
+    [Fact]
+    public async void TestCreating()
+    {
+        var command = new VendorCommand();
+        command.Action = CommandAction.Create;
+        command.Name = "Joe Blow";
+        command.Email = "joe.blow@email.com";
+
+        var result = await dispatcher.DispatchAsync(command);
+        Assert.Equal(1, result);
+    }
+
+    [Fact]
+    public async void TestUpdating()
+    {
+        var command = new VendorCommand();
+        command.Action = CommandAction.Update;
+        command.Id = 4;
+        command.Address = "Fake street";
+
+        var result = await dispatcher.DispatchAsync(command);
+        Assert.Equal(1, result);
+
+        var customer = await context.Vendors.FindAsync(4);
+        Assert.Equal("Fake street", customer!.Address);
+    }
+
+    [Fact]
+    public async void TestDeleting()
+    {
+        var command = new VendorCommand();
+        command.Action = CommandAction.Delete;
+        command.Id = 1;
+
+        var result = await dispatcher.DispatchAsync(command);
+        Assert.Equal(1, result);
+    }
+}
