cdb8029def5838f81a4bb6cb94ba6e96c5ecf7de
Mohammed Moussaoui <m.moussaoui@outlook.com>
2024-08-19 19:35:05 -0400

    Implement Customer entity querying - create CustomerQuery class - create CustomerQueryHandler class - add query test cases to CustomerTest class

---
+++ Commerce.Core/Sale/Handlers/CustomerQueryHandler.cs
@@ -0,0 +1,78 @@
+﻿using Commerce.Core.Common;
+using Commerce.Core.Sale.Entities;
+using Commerce.Core.Sale.Requests;
+using Commerce.Infrastructure.CQRS;
+using Microsoft.EntityFrameworkCore;
+
+namespace Commerce.Core.Sale.Handlers;
+
+public class CustomerQueryHandler : QueryHandler<CustomerQuery, Customer>
+{
+    private DataContext context;
+
+    public CustomerQueryHandler(DataContext context)
+    {
+        this.context = context;
+    }
+
+    protected override async Task<Customer?> FindAsync(CustomerQuery query, CancellationToken token)
+    {
+        if (query.Id > 0)
+        {
+            return await context.Customers.FindAsync(query.Id, token);
+        }
+
+        if (!string.IsNullOrEmpty(query.Email))
+        {
+            return await context.Customers.FirstOrDefaultAsync(customer => customer.Email!.ToLower() == query.Email.ToLower(), token);
+        }
+
+        if (!string.IsNullOrEmpty(query.Phone))
+        {
+            return await context.Customers.FirstOrDefaultAsync(customer => customer.Phone!.ToLower() == query.Phone.ToLower(), token);
+        }
+
+        return null;
+    }
+
+    protected override Task<List<Customer>> ListAsync(CustomerQuery query, CancellationToken token)
+    {
+        var customers = context.Customers.AsQueryable();
+        if (!string.IsNullOrEmpty(query.Name))
+        {
+            customers = customers.Where(customer => customer.Name.ToLower().Contains(query.Name.ToLower()));
+        }
+
+        if (!string.IsNullOrEmpty(query.Email))
+        {
+            customers = customers.Where(customer => customer.Email!.ToLower().Contains(query.Email.ToLower()));
+        }
+
+        if (!string.IsNullOrEmpty(query.Phone))
+        {
+            customers = customers.Where(customer => customer.Phone!.ToLower().Contains(query.Phone.ToLower()));
+        }
+
+        if (!string.IsNullOrEmpty(query.Locality))
+        {
+            customers = customers.Where(customer => customer.Locality!.ToLower().Contains(query.Locality.ToLower()));
+        }
+
+        if (!string.IsNullOrEmpty(query.Territory))
+        {
+            customers = customers.Where(customer => customer.Territory!.ToLower().Contains(query.Territory.ToLower()));
+        }
+
+        if (!string.IsNullOrEmpty(query.Country))
+        {
+            customers = customers.Where(customer => customer.Country!.ToLower().Contains(query.Country.ToLower()));
+        }
+
+        if (!string.IsNullOrEmpty(query.Sort))
+        {
+            customers = customers.Sort(query.Sort, query.Reverse);
+        }
+
+        return customers.Paginate(query.Page, query.Limit).ToListAsync(token);
+    }
+}
--- Commerce.Test/Sale/CustomerTest.cs
+++ Commerce.Test/Sale/CustomerTest.cs
@@ -23,11 +23,12 @@ public class CustomerTest
         dispatcher = new EventDispatcher();
         dispatcher.AddService(context);
         dispatcher.AddHandler<CustomerCommandHandler>();
+        dispatcher.AddHandler<CustomerQueryHandler>();
 
-        var customer1 = new Customer() { Id = 1, Name = "John Doe", Email = "john.doe@email.com" };
-        var customer2 = new Customer() { Id = 2, Name = "Jane Doe", Email = "Jane.doe@email.com" };
-        var customer3 = new Customer() { Id = 3, Name = "John Smith", Email = "Jane.Smith@email.com" };
-        var customer4 = new Customer() { Id = 4, Name = "Jane Smith", Email = "Jane.Smith@email.com" };
+        var customer1 = new Customer() { Id = 1, Name = "John Doe", Email = "john.doe@email.com", Country = "Canada" };
+        var customer2 = new Customer() { Id = 2, Name = "Jane Doe", Email = "Jane.doe@email.com", Country = "Canada" };
+        var customer3 = new Customer() { Id = 3, Name = "John Smith", Email = "Jane.Smith@email.com", Country = "USA" };
+        var customer4 = new Customer() { Id = 4, Name = "Jane Smith", Email = "Jane.Smith@email.com", Country = "USA" };
 
         context.Customers.Add(customer1);
         context.Customers.Add(customer2);
@@ -72,5 +73,89 @@ public class CustomerTest
 
         var result = await dispatcher.DispatchAsync(command);
         Assert.Equal(1, result);
+    }
+
+    [Fact]
+    public async void TestFinding()
+    {
+        var query = new CustomerQuery();
+        query.Action = QueryAction.Find;
+        query.Id = 1;
+
+        var customer = await dispatcher.DispatchAsync(query) as Customer;
+        Assert.NotNull(customer);
+        Assert.Equal("John Doe", customer.Name);
+    }
+
+    [Fact]
+    public async void TestFiltering()
+    {
+        var query = new CustomerQuery();
+        query.Action = QueryAction.List;
+        query.Country = "Canada";
+
+        var list = await dispatcher.DispatchAsync(query) as List<Customer>;
+        Assert.NotNull(list);
+        Assert.Equal(2, list.Count);
+    }
+
+    [Fact]
+    public async void TestSorting()
+    {
+        var query = new CustomerQuery();
+        query.Action = QueryAction.List;
+        query.Sort = "Name";
+
+        var list = await dispatcher.DispatchAsync(query) as List<Customer>;
+        Assert.NotNull(list);
+
+        var customer1 = list[0];
+        var customer2 = list[1];
+        var customer3 = list[2];
+        var customer4 = list[3];
+
+        Assert.Equal("Jane Doe", customer1.Name);
+        Assert.Equal("Jane Smith", customer2.Name);
+        Assert.Equal("John Doe", customer3.Name);
+        Assert.Equal("John Smith", customer4.Name);
+    }
+
+    [Fact]
+    public async void TestReverseSorting()
+    {
+        var query = new CustomerQuery();
+        query.Action = QueryAction.List;
+        query.Sort = "Name";
+        query.Reverse = true;
+
+        var list = await dispatcher.DispatchAsync(query) as List<Customer>;
+        Assert.NotNull(list);
+
+        var customer1 = list[0];
+        var customer2 = list[1];
+        var customer3 = list[2];
+        var customer4 = list[3];
+
+        Assert.Equal("John Smith", customer1.Name);
+        Assert.Equal("John Doe", customer2.Name);
+        Assert.Equal("Jane Smith", customer3.Name);
+        Assert.Equal("Jane Doe", customer4.Name);
+    }
+
+    [Fact]
+    public async void TestPaginating()
+    {
+        var query = new CustomerQuery();
+        query.Action = QueryAction.List;
+        query.Page = 2;
+        query.Limit = 2;
+
+        var list = await dispatcher.DispatchAsync(query) as List<Customer>;
+        Assert.NotNull(list);
+
+        var customer1 = list[0];
+        var customer2 = list[1];
+        Assert.Equal("John Smith", customer1.Name);
+        Assert.Equal("Jane Smith", customer2.Name);
     }
 }
