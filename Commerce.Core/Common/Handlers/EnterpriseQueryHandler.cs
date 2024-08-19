e69901d409145c74f1689e97e65b5e61e74a0806
Mohammed Moussaoui <m.moussaoui@outlook.com>
2024-08-19 19:24:14 -0400

    Implement Enterprise entity querying - create EnterpriseQuery class - create EnterpriseQueryHandler class - add query test cases to EnterpriseTest class

---
+++ Commerce.Core/Common/Handlers/EnterpriseQueryHandler.cs
@@ -0,0 +1,77 @@
+﻿using Commerce.Core.Common.Entities;
+using Commerce.Core.Common.Requests;
+using Commerce.Infrastructure.CQRS;
+using Microsoft.EntityFrameworkCore;
+
+namespace Commerce.Core.Common.Handlers;
+
+public class EnterpriseQueryHandler : QueryHandler<EnterpriseQuery, Enterprise>
+{
+    private DataContext context;
+
+    public EnterpriseQueryHandler(DataContext context)
+    {
+        this.context = context;
+    }
+
+    protected override async Task<Enterprise?> FindAsync(EnterpriseQuery query, CancellationToken token)
+    {
+        if (query.Id > 0)
+        {
+            return await context.Enterprises.FindAsync(query.Id, token);
+        }
+
+        if (!string.IsNullOrEmpty(query.Email))
+        {
+            return await context.Enterprises.FirstOrDefaultAsync(enterprise => enterprise.Email!.ToLower() == query.Email.ToLower(), token);
+        }
+
+        if (!string.IsNullOrEmpty(query.Phone))
+        {
+            return await context.Enterprises.FirstOrDefaultAsync(enterprise => enterprise.Phone!.ToLower() == query.Phone.ToLower(), token);
+        }
+
+        return null;
+    }
+
+    protected override Task<List<Enterprise>> ListAsync(EnterpriseQuery query, CancellationToken token)
+    {
+        var enterprises = context.Enterprises.AsQueryable();
+        if (!string.IsNullOrEmpty(query.Name))
+        {
+            enterprises = enterprises.Where(enterprise => enterprise.Name.ToLower().Contains(query.Name.ToLower()));
+        }
+
+        if (!string.IsNullOrEmpty(query.Email))
+        {
+            enterprises = enterprises.Where(enterprise => enterprise.Email!.ToLower().Contains(query.Email.ToLower()));
+        }
+
+        if (!string.IsNullOrEmpty(query.Phone))
+        {
+            enterprises = enterprises.Where(enterprise => enterprise.Phone!.ToLower().Contains(query.Phone.ToLower()));
+        }
+
+        if (!string.IsNullOrEmpty(query.Locality))
+        {
+            enterprises = enterprises.Where(enterprise => enterprise.Locality!.ToLower().Contains(query.Locality.ToLower()));
+        }
+
+        if (!string.IsNullOrEmpty(query.Territory))
+        {
+            enterprises = enterprises.Where(enterprise => enterprise.Territory!.ToLower().Contains(query.Territory.ToLower()));
+        }
+
+        if (!string.IsNullOrEmpty(query.Country))
+        {
+            enterprises = enterprises.Where(enterprise => enterprise.Country!.ToLower().Contains(query.Country.ToLower()));
+        }
+
+        if (!string.IsNullOrEmpty(query.Sort))
+        {
+            enterprises = enterprises.Sort(query.Sort, query.Reverse);
+        }
+
+        return enterprises.Paginate(query.Page, query.Limit).ToListAsync(token);
+    }
+}
--- Commerce.Test/Common/EnterpriseTest.cs
+++ Commerce.Test/Common/EnterpriseTest.cs
@@ -23,16 +23,17 @@ public class EnterpriseTest
         dispatcher = new EventDispatcher();
         dispatcher.AddService(context);
         dispatcher.AddHandler<EnterpriseCommandHandler>();
+        dispatcher.AddHandler<EnterpriseQueryHandler>();
 
-        var customer2 = new Enterprise() { Id = 1, Name = "Microsoft", Email = "contact@microsoft.com" };
-        var customer3 = new Enterprise() { Id = 2, Name = "Oracle", Email = "contact@email.com" };
-        var customer4 = new Enterprise() { Id = 3, Name = "Adobe", Email = "contact@adobe.com" };
-        var customer1 = new Enterprise() { Id = 4, Name = "Canonical", Email = "contact@canonical.com" };
+        var enterprise1 = new Enterprise() { Id = 1, Name = "Microsoft", Email = "contact@microsoft.com" };
+        var enterprise2 = new Enterprise() { Id = 2, Name = "Oracle", Email = "contact@oracle.com" };
+        var enterprise3 = new Enterprise() { Id = 3, Name = "Adobe", Email = "contact@adobe.com" };
+        var enterprise4 = new Enterprise() { Id = 4, Name = "Canonical", Email = "contact@canonical.com" };
 
-        context.Enterprises.Add(customer1);
-        context.Enterprises.Add(customer2);
-        context.Enterprises.Add(customer3);
-        context.Enterprises.Add(customer4);
+        context.Enterprises.Add(enterprise1);
+        context.Enterprises.Add(enterprise2);
+        context.Enterprises.Add(enterprise3);
+        context.Enterprises.Add(enterprise4);
         context.SaveChanges();
     }
 
@@ -59,8 +60,8 @@ public class EnterpriseTest
         var result = await dispatcher.DispatchAsync(command);
         Assert.Equal(1, result);
 
-        var customer = await context.Enterprises.FindAsync(4);
-        Assert.Equal("support@canonical.com", customer!.Email);
+        var enterprise = await context.Enterprises.FindAsync(4);
+        Assert.Equal("support@canonical.com", enterprise!.Email);
     }
 
     [Fact]
@@ -72,5 +73,89 @@ public class EnterpriseTest
 
         var result = await dispatcher.DispatchAsync(command);
         Assert.Equal(1, result);
+    }
+
+    [Fact]
+    public async void TestFinding()
+    {
+        var query = new EnterpriseQuery();
+        query.Action = QueryAction.Find;
+        query.Id = 1;
+
+        var enterprise = await dispatcher.DispatchAsync(query) as Enterprise;
+        Assert.NotNull(enterprise);
+        Assert.Equal("Microsoft", enterprise.Name);
+    }
+
+    [Fact]
+    public async void TestFiltering()
+    {
+        var query = new EnterpriseQuery();
+        query.Action = QueryAction.List;
+        query.Name = "Canonical";
+
+        var list = await dispatcher.DispatchAsync(query) as List<Enterprise>;
+        Assert.NotNull(list);
+        Assert.Single(list);
+    }
+
+    [Fact]
+    public async void TestSorting()
+    {
+        var query = new EnterpriseQuery();
+        query.Action = QueryAction.List;
+        query.Sort = "Name";
+
+        var list = await dispatcher.DispatchAsync(query) as List<Enterprise>;
+        Assert.NotNull(list);
+
+        var enterprise1 = list[0];
+        var enterprise2 = list[1];
+        var enterprise3 = list[2];
+        var enterprise4 = list[3];
+
+        Assert.Equal("Adobe", enterprise1.Name);
+        Assert.Equal("Canonical", enterprise2.Name);
+        Assert.Equal("Microsoft", enterprise3.Name);
+        Assert.Equal("Oracle", enterprise4.Name);
+    }
+
+    [Fact]
+    public async void TestReverseSorting()
+    {
+        var query = new EnterpriseQuery();
+        query.Action = QueryAction.List;
+        query.Sort = "Name";
+        query.Reverse = true;
+
+        var list = await dispatcher.DispatchAsync(query) as List<Enterprise>;
+        Assert.NotNull(list);
+
+        var enterprise1 = list[0];
+        var enterprise2 = list[1];
+        var enterprise3 = list[2];
+        var enterprise4 = list[3];
+
+        Assert.Equal("Oracle", enterprise1.Name);
+        Assert.Equal("Microsoft", enterprise2.Name);
+        Assert.Equal("Canonical", enterprise3.Name);
+        Assert.Equal("Adobe", enterprise4.Name);
+    }
+
+    [Fact]
+    public async void TestPaginating()
+    {
+        var query = new EnterpriseQuery();
+        query.Action = QueryAction.List;
+        query.Page = 2;
+        query.Limit = 2;
+
+        var list = await dispatcher.DispatchAsync(query) as List<Enterprise>;
+        Assert.NotNull(list);
+
+        var enterprise1 = list[0];
+        var enterprise2 = list[1];
+        Assert.Equal("Adobe", enterprise1.Name);
+        Assert.Equal("Canonical", enterprise2.Name);
     }
 }
