12a3eb72f0633996061fb072c964fc77b8035db7
Mohammed Moussaoui <m.moussaoui@outlook.com>
2025-05-10 20:45:18 +0200

    Rename the abstract class Ajustement to Memo

--- Commerce.Core/Purchase/Entities/DebitNote.cs
+++ Commerce.Core/Purchase/Entities/DebitNote.cs
@@ -2,7 +2,7 @@
 
 namespace Commerce.Core.Purchase.Entities;
 
-public class DebitNote : Adjustment<DebitLine, DebitLineTax>
+public class DebitNote : Memo<DebitLine, DebitLineTax>
 {
     public int SupplierId { get; set; }
 
--- Commerce.Core/Sale/Entities/CreditNote.cs
+++ Commerce.Core/Sale/Entities/CreditNote.cs
@@ -2,7 +2,7 @@
 
 namespace Commerce.Core.Sale.Entities;
 
-public class CreditNote : Adjustment<CreditLine, CreditLineTax>
+public class CreditNote : Memo<CreditLine, CreditLineTax>
 {
     public int CustomerId { get; set; }
 
